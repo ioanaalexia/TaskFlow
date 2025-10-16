edf919e1cdd7ad4bef56bfddfb0090704cde2aef
ioanaalexia <118600254+ioanaalexia@users.noreply.github.com>
2025-10-16 11:55:42 +0300

    Added Phase 1

---
+++ TaskFlow/Models/PersonalTask.cs
@@ -0,0 +1,16 @@
+﻿namespace TaskFlow.Models;
+
+public sealed class PersonalTask : TaskItem
+{   public string? Person { get; set; }
+
+    public PersonalTask(string title, string description, string? person = null) : base(title, description)
+    {
+        Person = person;
+    }
+
+    public override string Details()
+    {
+        var person = string.IsNullOrWhiteSpace(Person) ? "self" : Person;
+        return base.Details() + $"| For: {person}";
+    }
+}
